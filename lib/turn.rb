dccb0e2f0a8d29ba9ab703d58243f65f21757c50
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 19:22:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,7 @@ def display_board board
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 def input_to_index index
   index.to_i
   index = index - 1
