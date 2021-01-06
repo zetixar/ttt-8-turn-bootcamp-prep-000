db055ac0b51e06934e01e6ed1aa4a4f57514b866
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 18:55:03 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def display_board board
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+def input_to_index index
+  index = index - 1
+end
+
+def position_taken? board, index
+  if board[index] != "X" and "O"
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move? board, index
+  if !board[index].between(0,8)
+    return false
+  elsif position_taken?(board, index)
+    return false
+  else
+    return true
+  end
+end
+
+def move board, index, type = "X"
+  board[index] = type
+  
+end
+
+def turn board, index, type
+end
