7781db353f88cd0ef530377146e465a0093b5e99
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-06 19:27:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,9 +5,9 @@ def display_board board
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def input_to_index index
+def input_to_index input
 
-  index = index.to_i - 1
+  return input.to_i - 1
 end
 
 def position_taken? board, index
